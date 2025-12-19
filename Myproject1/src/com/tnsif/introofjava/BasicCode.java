cb15b052ee6277898892a941144d2985575dd4fe
lenovo <lenovo@VIJAYSWE>
2025-12-19 10:56:04 +0530

    Initial commit

--- Myproject1/src/com/tnsif/introofjava/BasicCode.java
+++ Myproject1/src/com/tnsif/introofjava/BasicCode.java
@@ -1,5 +1,29 @@
-package com.tnsif.introofjava;
+com.tnsif.introofjava;
 
 public class BasicCode {
+
+	public static void main(String[] args) {
+		
+		int age = 21;
+		double salary = 50000.00;
+		boolean isPassed = true;
+		char grade = 'A';
+		
+		double newSalary = age; //Implicit or internal conversion - Data widening
+		
+		int roundSalary = (int)salary; // Explicit or External - Data Narrowing
+		
+		//Arithmetic operation
+		int increment = 5000;
+		
+		int total = roundSalary + increment;
+		
+		
+		System.out.println(age);
+		System.out.println(salary);
+		System.out.println(roundSalary);
+		System.out.println(total);
+
+	}
 
 }
